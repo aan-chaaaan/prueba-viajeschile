bb02b46d47d9ade0ce792e1b8884137cc389091b
Angela <angela.valdebenito.v@gmail.com>
2021-05-13 15:57:20 -0400

    commit final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,32 @@
-$(function(){
+$(function () {
 
-    $(".link").click(function(event){
-    if (this.hash !== "") {
-        event.preventDefault();
-    
-        var hash = this.hash;
-    
-    $("html, body").animate({
-        scrollTop: $(hash).offset().top
-    }, 800, function(){
-    window.location.hash= hash;
-    });
-    }
+$(a).click(function(event){
+if (this.hash !== "") {
+event.preventDefault();
+        
+var hash = this.hash;
+        
+$("html, body").animate({
+scrollTop: $(hash).offset().top
+}, 800, function(){
+window.location.hash = hash;
+})
+}
+})
+
+$('[data-toggle="tooltip"]').tooltip()  
+ 
+$(".plane").click( function (){
+    $( ".text1" ).toggle();
     });
 
-    $('[data-toggle="tooltip"]').tooltip()
+$(".mountain").click( function (){
+    $( ".text2" ).toggle();
+    });    
+    
+$(".route").click( function (){
+    $( ".text3" ).toggle();
+    });
+
+
 });
