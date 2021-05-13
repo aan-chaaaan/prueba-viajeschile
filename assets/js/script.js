6491bcb6470f01ed9a34b77721ea863faee6c5e2
Angela <angela.valdebenito.v@gmail.com>
2021-05-12 20:24:39 -0400

    cambios cuatro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
-$(document).ready(function(){  
-    $('[data-toggle="tooltip"]').tooltip()
+$(function(){
 
-
-  $(".tarjeta1").click(function(){
-    $(".card-body").toggle();
+  $('.link').click(function(event){
+    if (this.hash !== "") {
+        event.preventDefault();
+    
+        var hash = this.hash;
+    
+    $("html, body").animate({
+        scrollTop: $(hash).offset().top
+    }, 800, function(){
+    window.location.hash= hash;
+    });
+    }
     });
 
-});
+}); 
 
+$('[data-toggle="tooltip"]').tooltip()
+
+    
+ 
